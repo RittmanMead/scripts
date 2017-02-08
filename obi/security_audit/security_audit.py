595650786e351d9ed2faf93445fab224a7a2c610
Nicholas Padgett <nicholas.padgett@rittmanmead.com>
2017-02-08 09:33:40 -0500

    Updated script.

--- obi/security_audit/security_audit.py
+++ obi/security_audit/security_audit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 """ Security mapping audit for OBIEE 12c. This code will generate a permissions report for all
 catalog objects, for the server upon which it is run. It takes the domain home as an argument in
@@ -32,7 +32,8 @@ lin_path = '/tmp/security_mappings.csv'
 
 
 # Run runcat script to generate csv permissions report - note report output in C:\
-def win_runcat():
+def win_runcat(domain_home):
+    os.chdir(domain_home + '\\bitools\\bin')
     os.system('runcat.cmd -cmd report -offline ' + domain_home +
               '/bidata/service_instances/ssi/metadata/content/catalog'
               ' -forceoutputFile ' + file_loc_win +
@@ -40,7 +41,8 @@ def win_runcat():
               ' -fields "Owner:Name:Path:ACL:Group Members" -delimiter ","')
 
 
-def lin_runcat():
+def lin_runcat(domain_home):
+    os.chdir(domain_home + '/bitools/bin')
     os.system('runcat.sh -cmd report -offline ' + domain_home +
               '/bidata/service_instances/ssi/metadata/content/catalog'
               ' -forceoutputFile ' + file_loc_lin +
@@ -48,7 +50,7 @@ def lin_runcat():
               ' -fields "Owner:Name:Path:ACL:Group Members" -delimiter ","')
 
 
-# Export cleaned up data_frames to server as csv
+# Export cleaned up dataframes to server as csv
 def exp_to_csv(exp_file, path):
     export = pd.DataFrame(exp_file)
     export.to_csv(path, index=False)
@@ -56,7 +58,7 @@ def exp_to_csv(exp_file, path):
 
 # Verify domain_home exists
 def verify_domain_home():
-    bi_domain = str(input('Please enter the OBIEE12c DOMAIN_HOME. \n>'))
+    bi_domain = input('Please enter the OBIEE12c DOMAIN_HOME. \n>')
     win_bi_tools = bi_domain + '\\bitools\\bin'
     lin_bi_tools = bi_domain + '/bitools/bin'
     if not os.path.exists(win_bi_tools) or not os.path.exists(lin_bi_tools):
@@ -78,19 +80,22 @@ def df_to_cleancsv(csv):
 
 
 if __name__ == '__main__':
-    domain_home = str(os.getenv('DOMAIN_HOME'))
-    if not os.path.exists(domain_home):
+    domain = os.environ.get('DOMAIN_HOME')
+
+    if domain is None:
         domain_home = verify_domain_home()
-        os.chdir(domain_home + '/bitools/bin')
+
     else:
-        os.chdir(domain_home + '/bitools/bin')  # change dir to bitools binaries to run runcat command
-    if platform.system() == 'Windows':  # check if OS is windows
-        win_runcat()
-        export_win = df_to_cleancsv(file_loc_win)  # Create dataframe from runcat output
-        os.remove(file_loc_win)  # Get rid of runcat csv output
-        exp_to_csv(export_win, win_path)  # Export dataframe to csv using function above
-    else:
-        lin_runcat()
-        export_lin = df_to_cleancsv(file_loc_lin)
-        os.remove(file_loc_lin)
-        exp_to_csv(export_lin, lin_path)
+        domain_home = domain
+
+        if platform.system() == 'Windows':  # check if OS is windows
+            win_runcat(domain_home)
+            export_win = df_to_cleancsv(file_loc_win)  # Create dataframe from runcat output
+            os.remove(file_loc_win)  # Get rid of runcat csv output
+            exp_to_csv(export_win, win_path)  # Export dataframe to csv using function above
+
+        else:
+            lin_runcat(domain_home)
+            export_lin = df_to_cleancsv(file_loc_lin)
+            os.remove(file_loc_lin)
+            exp_to_csv(export_lin, lin_path)
